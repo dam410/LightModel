a9ecc8d4ff116aaba5c386c399e3e05794a6f8f6
PC_ENCOV <dam410.m@gmail.com>
2022-02-15 12:00:29 +0100

    older source_line file

--- matlab/pose_estimation/source_line_from_planes.m
+++ matlab/pose_estimation/source_line_from_planes.m
@@ -9,10 +9,13 @@ function [L,err] = source_line_from_planes(planes)
 		V = eye(3);
 		S = eye(3);
 	end
+	S
 	L = plucker_dm_to_matrix(V(:,3),zeros(3,1));
 	err = S(3,3);
 	G = zeros(size(M));
 	figure;
 	plot3([G(1,:);M(1,:)],[G(2,:);M(2,:)],[G(3,:);M(3,:)],'-b');
+	hold on;
+	plot3([0,V(1,3)],[0,V(2,3)],[0,V(3,3)],'-r');
 	axis equal;
 end
